4ab5d41fe5d7e627d1c8cc45175bc15bb72d6a96
Gutemberg Ribeiro <gutemberg@outlook.com>
2019-10-19 14:50:20 -0500

    Remove legacy projects

--- src/Orleans.Http/Legacy/Execution/IMethodCallDispatcher.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace Orleans.Http.Execution
-{
-    public interface IMethodCallDispatcher
-    {
-        Task<object> Dispatch(IClusterClient client, MethodCall command);
-    }
-}
