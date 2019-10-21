8a3d4708b269ec67d1ebcc6205d0c2d07e6cf82b
Gutemberg Ribeiro <gutemberg@outlook.com>
2019-10-21 15:22:49 -0500

    Simple samples added

---
+++ Samples/OrleansHttp.Host/Startup.cs
@@ -0,0 +1,60 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Orleans.Http;
+
+namespace OrleansHttp.Host
+{
+    public class Startup
+    {
+        public const string SECRET = "THIS IS OUR AWESOME SUPER SECRET!!!";
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(opt =>
+            {
+                opt.RequireHttpsMetadata = false;
+                opt.SaveToken = true;
+                opt.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(SECRET)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+            services.AddAuthorization();
+
+            services
+                .AddGrainRouter()
+                .AddJsonMediaType()
+                .AddProtobufMediaType();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGrains("grains");
+            });
+        }
+    }
+}
---
+++ Samples/Readme.md
@@ -0,0 +1,8 @@
+# Samples
+
+This samples directory show a very basic Silo that expose a grain with a few methods which are mapped to endpoints.
+
+To run it, just run the `OrleansHttp.Host` project and it will listen for http on port 9090.
+
+Have a look on `IHelloGrain` which contains the methods with the GET url descriptions for the sample.
+
